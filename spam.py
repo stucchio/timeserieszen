c562e521d250e56a11e9393bc959722cc039c375
Chris Stucchio <stucchio@gmail.com>
2014-09-20 19:49:18 -0400

    java timestamps

--- spam.py
+++ spam.py
@@ -15,7 +15,7 @@ CARBON_PORT = 2003
 series = ['neha.sharma', 'zoe.saldana', 'foxy.cleopatra', 'server.load', 'server.ram']
 
 def datapoint():
-    return " ".join([random.choice(series), str(random.random()*100), str(int(time.time()))])
+    return " ".join([random.choice(series), str(random.random()*100), str(int(time.time()*1000))])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 for i in range(int(sys.argv[1])):
